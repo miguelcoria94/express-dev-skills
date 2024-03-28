85896e701f614b9caa40cf6a8412d508ab3630c5
miguelcoria94 <miguelcoria94@gmail.com>
2024-03-27 21:06:36 -0500

    completed part 2

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,6 +1,11 @@
 module.exports = {
   index,
-  getSkillById
+  getSkillById,
+  create,
+  createForm,
+  deleteOne,
+  update,
+  updateForm,
 };
 
 const skills = require("../models/skills");
@@ -12,5 +17,30 @@ function index(req, res, next) {
 
 function getSkillById(req, res, next) {
   const skill = skills.getOneSkill(req.params.id);
-  res.render("skills/id", { skill: skill, title:  `${skill.skill} Details` });
+  res.render("skills/id", { skill: skill, title: `${skill.skill}` });
+}
+
+function create(req, res, next) {
+  skills.create(req.body);
+  res.redirect("/skills");
+}
+
+function createForm(req, res, next) {
+  res.render("skills/create", { title: "Add Skill" });
+}
+
+
+function deleteOne(req, res, next) {
+  skills.deleteOne(req.params.id);
+  res.redirect("/skills");
+}
+
+function update(req, res, next) {
+  skills.update(req.params.id, req.body)
+  res.redirect("/skills");
+}
+
+function updateForm(req, res, next) {
+  const skill = skills.getOneSkill(req.params.id);
+  res.render("skills/id/edit", { skill, title: `Edit ${skill.skill}` });
 }
--- routes/skills.js
+++ routes/skills.js
@@ -1,10 +1,20 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const skillsCtrl = require('../controllers/skills');
+const skillsCtrl = require("../controllers/skills");
 
 /* GET users listing. */
-router.get('/', skillsCtrl.index);
+router.get("/", skillsCtrl.index);
 
-router.get('/:id', skillsCtrl.getSkillById)
+router.get("/create", skillsCtrl.createForm);
+
+router.get("/:id/edit", skillsCtrl.updateForm);
+
+router.delete("/:id", skillsCtrl.deleteOne);
+
+router.post("/", skillsCtrl.create);
+
+router.put("/:id", skillsCtrl.update);
+
+router.get("/:id", skillsCtrl.getSkillById);
 
 module.exports = router;
